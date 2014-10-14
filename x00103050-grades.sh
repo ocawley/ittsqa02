97bb926162463f5afbc94ea7b735a66dbfa8a790
x00103050 <x00103050@gmail.com>
2014-10-14 12:57:39 +0100

    Test Cases

---
+++ x00103050-grades.sh
@@ -0,0 +1,41 @@
+#Test Case 1
+#Inputs: 49 49
+#Expected Output: Fail
+#Actual: Fail
+#Outcome: True
+
+#//mint@mint-VirtualBox ~/ITT-Tallaght-Test $ java grades 49 49
+
+
+#Test Case 2
+#Inputs: 39 39
+#Expected Output: Component Fail
+#Actual : Component Fail
+#Outcome: True
+#
+#//mint@mint-VirtualBox ~/ITT-Tallaght-Test $ java grades 39 39
+
+
+#Test Case 3
+#Inputs: 60 60
+#Expected Output: Pass
+#Actual : Pass
+#Outcome: True
+#
+#//mint@mint-VirtualBox ~/ITT-Tallaght-Test $ java grades 60 60
+
+
+#Test Case 4
+#Inputs: 90 90
+#Expected Output: Pass with distinction
+#Actual : Pass with distinction
+#Outcome: True
+#
+#//mint@mint-VirtualBox ~/ITT-Tallaght-Test $ java grades 90 90
+
+#Test Case 5
+#Inputs: 68.5 44.8
+#Expected Output: Pass
+#Actual : For input string: "68.5"
+#Outcome: Fail
+#//mint@mint-VirtualBox ~/ITT-Tallaght-Test $ java grades 68.5 44.8
